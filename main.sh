291a12c89f72337ecc86235742a4127008b0baa3
Somasundaram <38272715+somasundarm@users.noreply.github.com>
2019-05-11 14:22:05 +0530

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,11 @@
+node {
+    stage('Hello') {
+        echo 'Hello World'
+    } 
+    stage('Do Something') {
+        echo 'Doing Something'
+    }
+    stage('Done') {
+        echo 'All Done'
+    }
+} 
