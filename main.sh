01cce02e019c43c2c871b846126121d288f2706d
Somasundaram <38272715+somasundarm@users.noreply.github.com>
2019-05-11 14:27:50 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,3 +1,16 @@
+package org.jenkinsci.plugins.pipeline.github.library;
+
+import hudson.Extension;
+import hudson.model.Job;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import jenkins.plugins.git.GitSCMSource;
+import org.jenkinsci.plugins.workflow.libs.LibraryConfiguration;
+import org.jenkinsci.plugins.workflow.libs.LibraryResolver;
+import org.jenkinsci.plugins.workflow.libs.SCMSourceRetriever
+
 node {
     stage('Hello') {
         echo 'Hello World'
